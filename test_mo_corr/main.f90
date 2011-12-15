fe0dffa1805f814b866e713422d20465d3f141f0
Matthias Cuntz <matthias.cuntz@ufz.de>
2011-12-15 20:59:49 +0000

    Added test_mo_corr
    git-svn-id: https://svn.ufz.de/svn/chs-svn/FORTRAN_chs_lib@139 20e3f476-f421-4892-ba25-73c5d2d2c40b

---
+++ test_mo_corr/main.f90
@@ -0,0 +1,59 @@
+PROGRAM main
+  
+  USE mo_kind,   ONLY: i4, dp, sp
+  USE mo_corr,   ONLY: autocoeffk, autocorr, corr, crosscoeffk, crosscorr
+  USE mo_moment, ONLY: mean, variance, covariance
+
+  IMPLICIT NONE
+  
+  INTEGER, PARAMETER :: n = 1500
+  REAL(dp), DIMENSION(n) :: dat1, dat2, dc
+  REAL(sp), DIMENSION(n) :: sat1, sat2, sc
+  INTEGER :: i, nout
+
+  LOGICAL :: isgood
+  
+  Write(*,*) ''
+  Write(*,*) 'Test mo_corr.f90'
+
+  ! Double precision
+  forall(i=1:n) dat1(i) = real(i,dp)
+  dat2 = dat1 + sin(dat1)
+  isgood = .true.
+  if (abs(variance(dat2)*real(n-1,dp)/real(n,dp) - autocoeffk(dat2,0)) > epsilon(1.0_dp)) &
+       isgood =.false.
+  if (abs(autocorr(dat2,0) - 1.0_dp) > epsilon(1.0_dp)) isgood =.false.
+  if (abs(covariance(dat1,dat2) - crosscoeffk(dat1,dat2,0))> epsilon(1.0_dp)) isgood =.false.
+  ! Covariance function: covariance(x,y) = corr(x,y)/n
+  call random_number(dat1)
+  dat2 = dat1 + sin(dat1)
+  dc = corr(dat1-mean(dat1), dat2-mean(dat2), nadjust=nout)
+  if (abs(dc(1)/real(nout,dp) - covariance(dat1,dat2)) > 0.01_dp) isgood =.false.
+
+  if (isgood) then
+     write(*,*) 'Double precision o.k.'
+  else
+     write(*,*) 'Double precision failed'
+  endif
+
+  ! Single precision
+  forall(i=1:n) sat1(i) = real(i,sp)
+  sat2 = sat1 + sin(sat1)
+  isgood = .true.
+  if (abs(variance(sat2)*real(n-1,sp)/real(n,sp) - autocoeffk(sat2,0)) > epsilon(1.0_sp)) &
+       isgood =.false.
+  if (abs(autocorr(sat2,0) - 1.0_sp) > epsilon(1.0_sp)) isgood =.false.
+  if (abs(covariance(sat1,sat2) - crosscoeffk(sat1,sat2,0))> epsilon(1.0_sp)) isgood =.false.
+  ! Covariance function: covariance(x,y) = corr(x,y)/n
+  call random_number(sat1)
+  sat2 = sat1 + sin(sat1)
+  sc = corr(sat1-mean(sat1), sat2-mean(sat2), nadjust=nout)
+  if (abs(sc(1)/real(nout,sp) - covariance(sat1,sat2)) > 0.01_sp) isgood =.false.
+
+  if (isgood) then
+     write(*,*) 'Single precision o.k.'
+  else
+     write(*,*) 'Single precision failed'
+  endif
+
+END PROGRAM main
