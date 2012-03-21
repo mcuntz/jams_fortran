d6ca995a972fc5e453be6b9ef468063e63e6b403
Juliena Mai <juliane.mai@ufz.de>
2012-03-21 18:18:34 +0000

    mo_histo: histo2d to generate histograms of 2d data
    git-svn-id: https://svn.ufz.de/svn/chs-svn/FORTRAN_chs_lib@227 20e3f476-f421-4892-ba25-73c5d2d2c40b

--- mo_histo.f90
+++ mo_histo.f90
@@ -16,11 +16,16 @@ MODULE mo_histo
 
   PRIVATE
 
-  PUBLIC :: histo           ! Histogram of data
+  PUBLIC :: histo           ! Histogram of 1D data
+  PUBLIC :: histo2d         ! Histogram of 2D data
 
   INTERFACE histo
      MODULE PROCEDURE histo_sp_1d, histo_dp_1d, histo_sp_2d, histo_dp_2d
   END INTERFACE histo
+  
+  INTERFACE histo2d
+     MODULE PROCEDURE histo2d_sp, histo2d_dp
+  END INTERFACE histo2d
 
   ! ------------------------------------------------------------------
 
@@ -68,6 +73,7 @@ CONTAINS
   !         real(sp/dp) :: binx(:)       x-values of the histogram (center of the bin)
   !       or
   !         real(sp/dp) :: binx(:,:)     center of the bins and the averaged 2nd, 3rd... coordinates
+  !       and
   !         integer(i4) :: bincount(:)   number of values within each bin
   !         real(sp/dp) :: width         width of a bin
 
@@ -139,6 +145,7 @@ CONTAINS
     INTEGER(I4)                              :: n        ! number of data points
     INTEGER(I4)                              :: k        ! number of bins
     REAL(DP)                                 :: w        ! binwidth
+    REAL(DP)                                 :: minimalvalue
     INTEGER(I4)                              :: i, binnr
     REAL(DP), DIMENSION(:),ALLOCATABLE       :: helpbinx
     INTEGER(I4), DIMENSION(:),ALLOCATABLE    :: helpbincount
@@ -182,14 +189,16 @@ CONTAINS
 
     allocate (binx(k),bincount(k))
 
+    minimalvalue=minval(x(:), mask=maske)
+
     do i=1,k
-       binx(i) = minval(x(:), mask=maske) + real(2*i-1,dp)/2.0_dp*w
+       binx(i) = minimalvalue + real(2*i-1,dp)/2.0_dp*w
        bincount(i) = 0_i4
     end do
 
     do i=1,size(x)
        If (maske(i)) then
-          binnr           = Floor((x(i)-minval(x(:), mask=maske))/w )
+          binnr           = Floor((x(i)-minimalvalue)/w )
           ! maxval(x) has to be assigned to last bin k not k+1
           binnr           = binnr - Floor(real(binnr,dp)/real(k,dp)) + 1_i4
           bincount(binnr) = bincount(binnr) + 1_i4
@@ -242,6 +251,7 @@ CONTAINS
     INTEGER(I4)                              :: n        ! number of data points
     INTEGER(I4)                              :: k        ! number of bins
     REAL(SP)                                 :: w        ! binwidth
+    REAL(SP)                                 :: minimalvalue
     INTEGER(I4)                              :: i, binnr
     REAL(SP), DIMENSION(:),ALLOCATABLE       :: helpbinx
     INTEGER(I4), DIMENSION(:),ALLOCATABLE    :: helpbincount
@@ -285,14 +295,16 @@ CONTAINS
 
     allocate (binx(k),bincount(k))
 
+    minimalvalue = minval(x(:), mask=maske)
+
     do i=1,k
-       binx(i) = minval(x(:), mask=maske) + real(2*i-1,sp)/2.0_sp*w
+       binx(i) = minimalvalue + real(2*i-1,sp)/2.0_sp*w
        bincount(i) = 0_i4
     end do
 
     do i=1,size(x)
        If (maske(i)) then
-          binnr           = Floor((x(i)-minval(x(:), mask=maske))/w )
+          binnr           = Floor((x(i)-minimalvalue)/w )
           ! maxval(x) has to be assigned to last bin k not k+1
           binnr           = binnr - Floor(real(binnr,sp)/real(k,sp)) + 1_i4
           bincount(binnr) = bincount(binnr) + 1_i4
@@ -344,6 +356,7 @@ CONTAINS
     INTEGER(I4)                              :: n        ! number of data points
     INTEGER(I4)                              :: k        ! number of bins
     REAL(DP)                                 :: w        ! binwidth
+    REAL(DP)                                 :: minimalvalue
     INTEGER(I4)                              :: i, j, binnr
     REAL(DP), DIMENSION(:,:),ALLOCATABLE     :: helpbinx
     INTEGER(I4), DIMENSION(:),ALLOCATABLE    :: helpbincount
@@ -386,16 +399,18 @@ CONTAINS
     ! Histogram
 
     allocate (binx(k,size(x,2)),bincount(k))
+    
+    minimalvalue = minval(x(:,1), mask=maske)
 
     binx = 0.0_dp
     bincount = 0_i4
     do i=1,k
-       binx(i,1) = minval(x(:,1), mask=maske) + real(2*i-1,dp)/2.0_dp*w
+       binx(i,1) = minimalvalue + real(2*i-1,dp)/2.0_dp*w
     end do
 
     do i=1,size(x,1)
        If (maske(i)) then
-          binnr           = Floor((x(i,1)-minval(x(:,1), mask=maske))/w )
+          binnr           = Floor((x(i,1)-minimalvalue)/w )
           ! maxval(x) has to be assigned to last bin k not k+1
           binnr           = binnr - Floor(real(binnr,dp)/real(k,dp)) + 1_i4
           bincount(binnr) = bincount(binnr) + 1_i4
@@ -462,6 +477,7 @@ CONTAINS
     INTEGER(I4)                              :: n        ! number of data points
     INTEGER(I4)                              :: k        ! number of bins
     REAL(SP)                                 :: w        ! binwidth
+    REAL(SP)                                 :: minimalvalue
     INTEGER(I4)                              :: i, j, binnr
     REAL(SP), DIMENSION(:,:),ALLOCATABLE     :: helpbinx
     INTEGER(I4), DIMENSION(:),ALLOCATABLE    :: helpbincount
@@ -504,16 +520,18 @@ CONTAINS
     ! Histogram
 
     allocate (binx(k,size(x,2)),bincount(k))
+    
+    minimalvalue = minval(x(:,1), mask=maske)
 
     binx = 0.0_sp
     bincount = 0_i4
     do i=1,k
-       binx(i,1) = minval(x(:,1), mask=maske) + real(2*i-1,sp)/2.0_sp*w
+       binx(i,1) = minimalvalue + real(2*i-1,sp)/2.0_sp*w
     end do
 
     do i=1,size(x,1)
        If (maske(i)) then
-          binnr           = Floor((x(i,1)-minval(x(:,1), mask=maske))/w )
+          binnr           = Floor((x(i,1)-minimalvalue)/w )
           ! maxval(x) has to be assigned to last bin k not k+1
           binnr           = binnr - Floor(real(binnr,sp)/real(k,sp)) + 1_i4
           bincount(binnr) = bincount(binnr) + 1_i4
@@ -562,5 +580,318 @@ CONTAINS
   END SUBROUTINE histo_sp_2d
 
   ! ------------------------------------------------------------------
+
+  !     NAME
+  !         histo2d
+
+  !     PURPOSE
+  !         Calculates the 2D histogram of n data 2D points (Xi,Yi),i=1,n , 
+  !         i.e. the n data points are sorted into k x k categories (bins).  
+  !
+  !         With no optional arguments the number of bins k per dimension 
+  !         is the integer part of n**(1/4) where n is the number of data points. 
+  !         E.g. n=20 will create k=2 bins in each direction (= 4 bins in sum).
+  !         The bin width w is calculated depending on the minimal and maximal value of 
+  !         the first coordinate):
+  !                     w = (maxval(X) - minval(X)) / k
+  !
+  !         Optionally, one can fix the number of bins k (integer i4) 
+  !         while the width of the bins is determined. 
+  !         Or, on can fix the binwidth w (float sp/dp) while k is determined automatically.
+  !         If an optional mask is given, the histogram is taken into account only data points  
+  !         (Xi, Yi) which have a true value in the mask.
+  !         (Xi, Yi) can be single or double precision. 
+  !         The result will have the same numerical precision.
+
+  !     CALLING SEQUENCE
+  !     with no optional arguments
+  !         call hist2d(x, binx, bincount, width)  
+  !     to fix the number of bins k
+  !         call hist2d(x, binx, bincount, width, bins=k,     mask=maske)  
+  !     to fix the bin width w
+  !         call hist2d(x, binx, bincount, width, binwidth=w, mask=maske)  
+  
+  !     INDENT(IN)
+  !         real(sp/dp) :: x(:,2)   2D array of (x,y) values
+
+  !     INDENT(INOUT)
+  !         None
+
+  !     INDENT(OUT)
+  !         real(sp/dp) :: binx(:,2)     (x,y)-values of the histogram (center of the bin)
+  !         integer(i4) :: bincount(:)   number of values within each bin
+  !         real(sp/dp) :: width         width of a bin
+
+  !     INDENT(IN), OPTIONAL
+  !         logical     :: mask(:)      1D-array of logical values with size(x,1).
+  !                                     If present, only the data points in (Xij) corresponding 
+  !                                     to the true values in mask are used.
+  !         integer(i4) :: bins         Number of bins to be generated.
+  !                                     If present, the number of bins will be fixed to this value.
+  !                                     Otherwise, is set to integer part of sqrt(size(x)) or 
+  !                                     using the optionally set binwidth.
+  !         real(sp/dp) :: binwidth(2)  Width of the bins to be generated. 
+  !                                     One value for each direction.
+  !                                     If present, the width of bins will be fixed to this value
+  !                                     and the INDENT(OUT) width will be set to this value.
+  !                                     Otherwise, will be determined using the number of bins.  
+
+  !     INDENT(INOUT), OPTIONAL
+  !         None
+
+  !     INDENT(OUT), OPTIONAL
+  !         None
+
+  !     RESTRICTIONS
+  !         The number of bins is equal in each direction, i.e. x and y.
+  !         Input values must be floating points.
+  !         Use more than one data point: Size(x,1) > 1.
+  !         If you fix the number of bins, it has to be larger than 1: bins > 1.
+  !         If you fix the width of the bins, it has to be larger than 0: binwidth > 0.
+  !         Either the number of bins or the width of the bins can be fixed, not both.
+  !         Not all values of the first coordinate should be equal.
+  !         Dimension of x and mask have to match: Size(x,1) = Size(mask). 
+
+  !     EXAMPLE
+  !     REAL(DP),    DIMENSION(4,2)              :: x
+  !     REAL(DP),    DIMENSION(:,:), ALLOCATABLE :: testbinx
+  !     INTEGER(I4), DIMENSION(:),   ALLOCATABLE :: testbincount
+  !     REAL(DP),    DIMENSION(2)                :: testwidth
+  
+  !     x(1,:) = (/3.0_dp, 1.0_dp/)
+  !     x(2,:) = (/6.0_dp, 4.0_dp/)
+  !     x(3,:) = (/2.0_dp, 3.0_dp/) 
+  !     x(4,:) = (/1.0_dp, 1.0_dp/)
+  !     
+  !     call histo2d(x, testbinx, testbincount, testwidth) 
+  !
+  !         ^
+  !         |                                      x ... data points
+  !       4 |  ********************x               * ... edge of bin
+  !         |  *         *         *
+  !       3 |  *   x     *         *
+  !         |  *********************
+  !       2 |  *         *         *
+  !         |  *         *         *
+  !       1 |  x*******x************
+  !         |
+  !         |- - - - - - - - - - - - -  >
+  !            1   2   3   4   5   6
+  !
+  !     print*, 'testbinx(1)  = ',testbinx(1,:)    --> (/2.25, 1.75/)
+  !     print*, 'testbinx(2)  = ',testbinx(2,:)    --> (/4.75, 1.75/)
+  !     print*, 'testbinx(3)  = ',testbinx(3,:)    --> (/2.25, 3.25/)
+  !     print*, 'testbinx(4)  = ',testbinx(4,:)    --> (/4.75, 3.25/)
+  !     print*, 'testbincount = ',testbincount     --> (/ 2, 0, 1, 1/)
+  !     print*, 'testwidth    = ',testwidth        --> (/ 2.5, 1.5 /)
+  !
+  !     LITERATURE
+  !         
+  !     HISTORY
+  !         Written,  Juliane Mai, Mar 2012
+  !         Modified, 
+
+  SUBROUTINE histo2d_dp(x, binx, bincount, width, mask, bins, binwidth)
+
+    IMPLICIT NONE
+
+    REAL(DP),    DIMENSION(:,:),                   INTENT(IN)  :: x
+    REAL(DP),    DIMENSION(:,:),      ALLOCATABLE, INTENT(OUT) :: binx
+    INTEGER(I4), DIMENSION(:),        ALLOCATABLE, INTENT(OUT) :: bincount
+    REAL(DP),    DIMENSION(2),                     INTENT(OUT) :: width
+
+    INTEGER(I4),               OPTIONAL,           INTENT(IN)  :: bins
+    REAL(DP),    DIMENSION(2), OPTIONAL,           INTENT(IN)  :: binwidth
+    LOGICAL,     DIMENSION(:), OPTIONAL,           INTENT(IN)  :: mask
+
+    ! Local variables
+    LOGICAL, DIMENSION(size(x,1))            :: maske
+    INTEGER(I4)                              :: n        ! number of data points
+    INTEGER(I4)                              :: k        ! number of bins
+    REAL(DP), DIMENSION(2)                   :: w        ! binwidth
+    REAL(DP), DIMENSION(2)                   :: minimalvalue
+    INTEGER(I4)                              :: i, j
+    INTEGER(I4)                              :: binnr, binnr1, binnr2
+    !REAL(DP), DIMENSION(:),ALLOCATABLE       :: helpbinx
+    !INTEGER(I4), DIMENSION(:),ALLOCATABLE    :: helpbincount
+
+    if (present(mask)) then
+       if (size(mask) /= size(x,1)) stop 'Error histo_dp: size(mask) /= size(x)'
+       maske = mask
+       n = count(maske)
+    else
+       maske(:) = .true.
+       n = size(x,1)
+    endif
+
+    if (n .le. (1.0_dp+tiny(1.0_dp))) then
+       stop 'Error histo2d_dp: size(x) must be at least 2'
+    end if
+    !if (maxval(x(:), mask=maske)-minval(x(:), mask=maske) <= tiny(1.0_dp)) then
+    !   stop 'Error histo_dp: all entries of x(:) are equal'
+    !end if
+
+    if (present(bins) .and. present(binwidth)) then
+       stop 'Error histo2d_dp: Either fix number of bins or binwidth, not both.' 
+    end if 
+
+    if (present(bins)) then
+       if (bins .le. 1_i4) stop 'Error histo2d_dp: number of bins <= 1'
+       k = bins
+       w(1) = (maxval(x(:,1), mask=maske)-minval(x(:,1), mask=maske)) / real(k,dp)
+       w(2) = (maxval(x(:,2), mask=maske)-minval(x(:,2), mask=maske)) / real(k,dp)
+    else
+       if (present(binwidth)) then
+          if (any(binwidth .le. tiny(1.0_dp))) stop 'Error histo2d_dp: width of bins too small'
+          if (size(binwidth,1) .ne. 2_i4) stop 'Error histo2d_dp: size(width) has to be 2'
+          w(:) = binwidth(:)
+          k = Max( Ceiling( (maxval(x(:,1), mask=maske)-minval(x(:,1), mask=maske)) / w(1) ), &
+                   Ceiling( (maxval(x(:,2), mask=maske)-minval(x(:,2), mask=maske)) / w(2) ) )
+       else
+          k = Floor(real(n,dp)**0.25_dp)
+          w(1) = (maxval(x(:,1), mask=maske)-minval(x(:,1), mask=maske)) / real(k,dp)
+          w(2) = (maxval(x(:,2), mask=maske)-minval(x(:,2), mask=maske)) / real(k,dp)
+       end if
+    endif
+
+    ! Histogram
+
+    allocate (binx(k*k,2),bincount(k*k))
+
+    minimalvalue(1)=minval(x(:,1), mask=maske)
+    minimalvalue(2)=minval(x(:,2), mask=maske)
+
+    do i=1,k
+       do j=1,k
+          binx((i-1)*k+j,1)   = minimalvalue(1) + real(2*j-1,dp)/2.0_dp*w(1)
+          binx((i-1)*k+j,2)   = minimalvalue(2) + real(2*i-1,dp)/2.0_dp*w(2)
+          bincount((i-1)*k+j) = 0_i4
+       end do
+    end do
+
+    do i=1,size(x,1)
+       If (maske(i)) then
+          !print*, 'x(i) = ',x(i,:)
+          binnr1 =   Floor((x(i,1)-minimalvalue(1))/w(1) )
+          ! maxval(x) has to be assigned to last bin k not k+1
+          binnr1 =   binnr1 - Floor(real(binnr1,dp)/real(k,dp)) + 1_i4
+          !print*, 'i = ',i,'  binnr 1 = ',binnr1
+          
+          binnr2 =   Floor((x(i,2)-minimalvalue(2))/w(2) )
+          ! maxval(x) has to be assigned to last bin k not k+1
+          binnr2 =   binnr2 - Floor(real(binnr2,dp)/real(k,dp)) + 1_i4
+          !print*, 'i = ',i,'  binnr 2 = ',binnr2
+          
+          binnr           = (binnr2-1_i4)*k + binnr1
+          !print*, 'i = ',i,'  binnr 3 = ',binnr
+          bincount(binnr) = bincount(binnr) + 1_i4
+       end if
+    end do
+
+    width(:) = w(:)
+
+  END SUBROUTINE histo2d_dp
+
+  
+ SUBROUTINE histo2d_sp(x, binx, bincount, width, mask, bins, binwidth)
+
+    IMPLICIT NONE
+
+    REAL(SP),    DIMENSION(:,:),                   INTENT(IN)  :: x
+    REAL(SP),    DIMENSION(:,:),      ALLOCATABLE, INTENT(OUT) :: binx
+    INTEGER(I4), DIMENSION(:),        ALLOCATABLE, INTENT(OUT) :: bincount
+    REAL(SP),    DIMENSION(2),                     INTENT(OUT) :: width
+
+    INTEGER(I4),               OPTIONAL,           INTENT(IN)  :: bins
+    REAL(SP),    DIMENSION(2), OPTIONAL,           INTENT(IN)  :: binwidth
+    LOGICAL,     DIMENSION(:), OPTIONAL,           INTENT(IN)  :: mask
+
+    ! Local variables
+    LOGICAL, DIMENSION(size(x,1))            :: maske
+    INTEGER(I4)                              :: n        ! number of data points
+    INTEGER(I4)                              :: k        ! number of bins
+    REAL(SP), DIMENSION(2)                   :: w        ! binwidth
+    REAL(SP), DIMENSION(2)                   :: minimalvalue
+    INTEGER(I4)                              :: i, j
+    INTEGER(I4)                              :: binnr, binnr1, binnr2
+    !REAL(SP), DIMENSION(:),ALLOCATABLE       :: helpbinx
+    !INTEGER(I4), DIMENSION(:),ALLOCATABLE    :: helpbincount
+
+    if (present(mask)) then
+       if (size(mask) /= size(x,1)) stop 'Error histo_sp: size(mask) /= size(x)'
+       maske = mask
+       n = count(maske)
+    else
+       maske(:) = .true.
+       n = size(x,1)
+    endif
+
+    if (n .le. (1.0_sp+tiny(1.0_sp))) then
+       stop 'Error histo2d_sp: size(x) must be at least 2'
+    end if
+    !if (maxval(x(:), mask=maske)-minval(x(:), mask=maske) <= tiny(1.0_sp)) then
+    !   stop 'Error histo_sp: all entries of x(:) are equal'
+    !end if
+
+    if (present(bins) .and. present(binwidth)) then
+       stop 'Error histo2d_sp: Either fix number of bins or binwidth, not both.' 
+    end if 
+
+    if (present(bins)) then
+       if (bins .le. 1_i4) stop 'Error histo2d_sp: number of bins <= 1'
+       k = bins
+       w(1) = (maxval(x(:,1), mask=maske)-minval(x(:,1), mask=maske)) / real(k,sp)
+       w(2) = (maxval(x(:,2), mask=maske)-minval(x(:,2), mask=maske)) / real(k,sp)
+    else
+       if (present(binwidth)) then
+          if (any(binwidth .le. tiny(1.0_sp))) stop 'Error histo2d_sp: width of bins too small'
+          if (size(binwidth,1) .ne. 2_i4) stop 'Error histo2d_sp: size(width) has to be 2'
+          w(:) = binwidth(:)
+          k = Max( Ceiling( (maxval(x(:,1), mask=maske)-minval(x(:,1), mask=maske)) / w(1) ), &
+                   Ceiling( (maxval(x(:,2), mask=maske)-minval(x(:,2), mask=maske)) / w(2) ) )
+       else
+          k = Floor(Sqrt(real(n,sp)))
+          w(1) = (maxval(x(:,1), mask=maske)-minval(x(:,1), mask=maske)) / real(k,sp)
+          w(2) = (maxval(x(:,2), mask=maske)-minval(x(:,2), mask=maske)) / real(k,sp)
+       end if
+    endif
+
+    ! Histogram
+
+    allocate (binx(k*k,2),bincount(k*k))
+
+    minimalvalue(1)=minval(x(:,1), mask=maske)
+    minimalvalue(2)=minval(x(:,2), mask=maske)
+
+    do i=1,k
+       do j=1,k
+          binx((i-1)*k+j,1)   = minimalvalue(1) + real(2*j-1,sp)/2.0_sp*w(1)
+          binx((i-1)*k+j,2)   = minimalvalue(2) + real(2*i-1,sp)/2.0_sp*w(2)
+          bincount((i-1)*k+j) = 0_i4
+       end do
+    end do
+
+    do i=1,size(x,1)
+       If (maske(i)) then
+          !print*, 'x(i) = ',x(i,:)
+          binnr1 =   Floor((x(i,1)-minimalvalue(1))/w(1) )
+          ! maxval(x) has to be assigned to last bin k not k+1
+          binnr1 =   binnr1 - Floor(real(binnr1,sp)/real(k,sp)) + 1_i4
+          !print*, 'i = ',i,'  binnr 1 = ',binnr1
+          
+          binnr2 =   Floor((x(i,2)-minimalvalue(2))/w(2) )
+          ! maxval(x) has to be assigned to last bin k not k+1
+          binnr2 =   binnr2 - Floor(real(binnr2,sp)/real(k,sp)) + 1_i4
+          !print*, 'i = ',i,'  binnr 2 = ',binnr2
+          
+          binnr           = (binnr2-1_i4)*k + binnr1
+          !print*, 'i = ',i,'  binnr 3 = ',binnr
+          bincount(binnr) = bincount(binnr) + 1_i4
+       end if
+    end do
+
+    width(:) = w(:)
+
+  END SUBROUTINE histo2d_sp
 
 END MODULE mo_histo
