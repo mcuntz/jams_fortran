f1475f16ae681ac0d993ec27e8d1ce5a9f78d314
Juliena Mai <juliane.mai@ufz.de>
2012-07-30 19:34:31 +0000

    mo_asa047 renamed (now mo_nelmin)
    git-svn-id: https://svn.ufz.de/svn/chs-svn/FORTRAN_chs_lib@344 20e3f476-f421-4892-ba25-73c5d2d2c40b

---
+++ test/test_mo_nelmin/rosenbrock.f90
@@ -0,0 +1,53 @@
+function rosenbrock(x)
+
+!*****************************************************************************80
+!
+!! ROSENBROCK evaluates the Rosenbrock parabolic value function.
+!
+!  Licensing:
+!
+!    This code is distributed under the GNU LGPL license.
+!
+!  Modified:
+!
+!    19 February 2008
+!
+!  Author:
+!
+!    John Burkardt
+!
+!  Reference:
+!
+!    R ONeill,
+!    Algorithm AS 47:
+!    Function Minimization Using a Simplex Procedure,
+!    Applied Statistics,
+!    Volume 20, Number 3, 1971, pages 338-345.
+!
+!  Parameters:
+!
+!    Input, real(dp) X(2), the argument.
+!
+!    Output, real(dp) ROSENBROCK, the value of the function.
+!
+  use mo_kind,   only: dp
+
+  implicit none
+
+  real(dp), dimension(:), intent(in) :: x
+  real(dp) :: rosenbrock
+
+  real(dp) :: fx
+  real(dp) :: fx1
+  real(dp) :: fx2
+
+  fx1 = x(2) - x(1) * x(1)
+  fx2 = 1.0_dp - x(1)
+
+  fx = 100.0_dp * fx1 * fx1 + fx2 * fx2
+
+  rosenbrock = fx
+
+  return
+
+end function rosenbrock
