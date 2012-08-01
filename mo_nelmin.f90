1dc37e902c6738fc3058e9f414f9d0480d00a3bd
Juliena Mai <juliane.mai@ufz.de>
2012-08-01 18:07:20 +0000

    FORTRAN_lib: mo_nelmin: added nelminrange for fixing search domain (needed especially for functions with no slope)
    git-svn-id: https://svn.ufz.de/svn/chs-svn/FORTRAN_chs_lib@355 20e3f476-f421-4892-ba25-73c5d2d2c40b

--- mo_nelmin.f90
+++ mo_nelmin.f90
@@ -33,6 +33,7 @@ MODULE mo_nelmin
 
   PUBLIC :: nelmin          ! minimizes a function using the Nelder-Mead algorithm
   PUBLIC :: nelminxy        ! same as nelmin but pass x and y to function
+  PUBLIC :: nelminrange     ! same as nelmin but with given range of parameter
 
   ! ------------------------------------------------------------------
 
@@ -43,6 +44,7 @@ CONTAINS
   !     NAME
   !         nelmin
   !         nelminxy
+  !         nelminrange
 
   !     PURPOSE
   !         Minimizes a user-specified function using the Nelder-Mead algorithm.
@@ -79,12 +81,15 @@ CONTAINS
   !                       neval, numrestart, ierror)
   !         pmin = nelminxy(func, pstart, xx, yy, funcmin, varmin, step, konvge, maxeval, &
   !                         neval, numrestart, ierror)
+  !         pmin = nelmin(func, pstart, prange, funcmin, varmin, step, konvge, maxeval, &
+  !                       neval, numrestart, ierror)
 
   !     INDENT(IN)
   !         real(dp) :: func(p,xx,yy)    Function on which to search the minimum
   !         real(dp) :: pstart(:)        Starting point for the iteration.
   !         real(dp) :: xx               First values to pass as function arguments
   !         real(dp) :: yy               Second values to pass as function arguments
+  !         real(dp) :: prange(:,2)      Range of parameters (upper and lower bound).
 
   !     INDENT(INOUT)
   !         None
@@ -140,7 +145,8 @@ CONTAINS
   !         FORTRAN90 version by John Burkardt.
   !         Modified,  Matthias Cuntz, Jul 2012 - i4, dp, intent
   !                                             - function, optional
-  !                                             - nelimxy
+  !                                             - nelminxy
+  !                    Juliane Mai,    Aug 2012 - nelminrange
 
   function nelmin(func, pstart, funcmin, varmin, step, konvge, maxeval, &
        neval, numrestart, ierror)
@@ -779,6 +785,343 @@ CONTAINS
     endif
 
   end function nelminxy
+
+  function nelminrange(func, pstart, prange, funcmin, varmin, step, konvge, maxeval, &
+       neval, numrestart, ierror)
+
+    implicit none
+
+    INTERFACE
+       FUNCTION func(pp)
+         USE mo_kind, ONLY: dp
+         IMPLICIT NONE
+         REAL(dp), DIMENSION(:), INTENT(IN) :: pp
+         REAL(dp) :: func
+       END FUNCTION func
+    END INTERFACE
+    real(dp),              intent(IN)  :: pstart(:)
+    real(dp),              intent(IN)  :: prange(:,:)
+    real(dp),    optional, intent(OUT) :: funcmin
+    real(dp),    optional, intent(IN)  :: varmin
+    real(dp),    optional, intent(IN)  :: step(:)
+    integer(i4), optional, intent(IN)  :: konvge
+    integer(i4), optional, intent(IN)  :: maxeval
+    integer(i4), optional, intent(OUT) :: neval
+    integer(i4), optional, intent(OUT) :: numrestart
+    integer(i4), optional, intent(OUT) :: ierror
+    real(dp)                           :: nelminrange(size(pstart))
+
+    real(dp), parameter :: ccoeff = 0.5_dp
+    real(dp), parameter :: ecoeff = 2.0_dp
+    real(dp), parameter :: rcoeff = 1.0_dp
+    real(dp), parameter :: eps    = 0.001_dp
+    real(dp) :: ipstart(size(pstart))
+    real(dp) :: ifuncmin
+    real(dp) :: ivarmin
+    real(dp) :: istep(size(pstart))
+    integer(i4) :: ikonvge
+    integer(i4) :: imaxeval
+    integer(i4) :: ineval
+    integer(i4) :: inumrestart
+    integer(i4) :: iierror
+    integer(i4) :: n, nn
+    integer(i4) :: i
+    integer(i4) :: ihi
+    integer(i4) :: ilo
+    integer(i4) :: j
+    integer(i4) :: jcount
+    integer(i4) :: l
+    real(dp) :: del
+    real(dp) :: p(size(pstart),size(pstart)+1)
+    real(dp) :: p2star(size(pstart))
+    real(dp) :: pbar(size(pstart))
+    real(dp) :: pstar(size(pstart))
+    real(dp) :: rq
+    real(dp) :: x
+    real(dp) :: y(size(pstart)+1)
+    real(dp) :: y2star
+    real(dp) :: ylo
+    real(dp) :: ystar
+    real(dp) :: z
+    real(dp) :: dn, dnn
+    real(dp) :: p0(size(pstart)), y0
+    !
+    ! Defaults
+    !
+    nelminrange(:) = 0.5_dp * ( prange(:,1) + prange(:,2) )
+
+    if (present(varmin)) then
+       if (varmin <= 0.0_dp) stop 'Error nelim: varmin<0'
+       ivarmin = varmin
+    else
+       ivarmin = 1.0e-9_dp
+    endif
+    if (present(maxeval)) then
+       if (maxeval <= 1) stop 'Error nelim: maxeval<=1'
+       imaxeval = maxeval
+    else
+       imaxeval = 1000
+    endif
+    if (present(konvge)) then
+       ikonvge = konvge
+    else
+       ikonvge = imaxeval / 10
+    endif
+    if (ikonvge < 1) stop 'Error nelim: konvg<1'
+    !
+    if (present(step)) then
+       istep = step
+    else ! if not given, deviate initial by 1%
+       y0 = func(pstart)
+       do i=1, size(pstart)
+          p0 = pstart
+          if (p0(i) /= 0.0_dp) then
+             ! bound to range
+             p0(i) = min( prange(i,2) , max( prange(i,1) , 1.01_dp*p0(i) ) )
+          else
+             ! bound to range
+             p0(i) = min( prange(i,2) , max( prange(i,1) , 0.01_dp*p0(i) ) )
+          endif
+          istep(i) = abs(func(p0) - y0)
+       enddo
+    endif
+    !
+    !  Check the input parameters.
+    !
+    if (size(prange,2) .ne. 2_i4)           stop 'nelminrange: range has to be array of size (:,2)'
+    if (size(prange,1) .ne.  size(ipstart)) stop 'nelminrange: range has to be given for each dimension'
+    if (.not. (all(prange(:,1) .le. pstart(:)) .and. all(prange(:,2) .ge. pstart(:)) )) then
+       stop 'nelminrange: starting point is not in range'
+    end if
+    !
+    !  Initialization.
+    !
+    ipstart = pstart
+    n       = size(ipstart)
+    ineval  = 0_i4
+    inumrestart = 0_i4
+    jcount = ikonvge
+    dn     = real(n, dp)
+    nn     = n + 1
+    dnn    = real(nn, dp)
+    del    = 1.0_dp
+    rq     = ivarmin * dn
+    !
+    !  Initial or restarted loop.
+    !
+    do
+       p(1:n,nn) = ipstart(1:n)
+       y(nn)     = func(ipstart)
+       ineval     = ineval + 1
+       !
+       !  Define the initial simplex.
+       !
+       do j = 1, n
+          x          = ipstart(j)
+          ! bound to range
+          ipstart(j) =  min( prange(j,2) , max( prange(j,1) , ipstart(j) + istep(j) * del ) )
+          p(1:n,j)   = ipstart(1:n)
+          y(j)       = func(ipstart)
+          ineval     = ineval + 1
+          ipstart(j) = x
+       end do
+       !
+       !  Find highest and lowest Y values.  FUNCMIN = Y(IHI) indicates
+       !  the vertex of the simplex to be replaced.
+       !
+       ilo = minloc(y(1:n+1), 1)
+       ylo = y(ilo)
+       !
+       !  Inner loop.
+       !
+       do while ( ineval < imaxeval )
+          !
+          !  FUNCMIN is, of course, the HIGHEST value???
+          !
+          ihi     = maxloc(y(1:n+1), 1)
+          ifuncmin = y(ihi)
+          !
+          !  Calculate PBAR, the centroid of the simplex vertices
+          !  excepting the vertex with Y value FUNCMIN.
+          !
+          do i = 1, n
+             pbar(i) = ( sum(p(i,1:n+1)) - p(i,ihi) ) / dn
+          end do
+          !
+          !  Reflection through the centroid.
+          !
+          ! bound to range
+          pstar(1:n) = min( prange(1:n,2) , max( prange(1:n,1) , pbar(1:n) + rcoeff * ( pbar(1:n) - p(1:n,ihi) ) ) )  
+          ystar      = func(pstar)
+          ineval      = ineval + 1
+          !
+          !  Successful reflection, so extension.
+          !
+          if ( ystar < ylo ) then
+
+             ! bound to range
+             p2star(1:n) = min( prange(1:n,2) , max( prange(1:n,1) , pbar(1:n) + ecoeff * ( pstar(1:n) - pbar(1:n) ) ) )
+             y2star      = func(p2star)
+             ineval       = ineval + 1
+             !
+             !  Retain extension or contraction.
+             !
+             if ( ystar < y2star ) then
+                p(1:n,ihi) = pstar(1:n)
+                y(ihi)     = ystar
+             else
+                p(1:n,ihi) = p2star(1:n)
+                y(ihi)     = y2star
+             end if
+             !
+             !  No extension.
+             !
+          else
+             l = 0
+             do i = 1, nn
+                if ( ystar < y(i) ) l = l + 1
+             end do
+
+             if ( 1 < l ) then
+                p(1:n,ihi) = pstar(1:n)
+                y(ihi)     = ystar
+                !
+                !  Contraction on the Y(IHI) side of the centroid.
+                !
+             else if ( l == 0 ) then
+                ! bound to range
+                p2star(1:n)  = min( prange(1:n,2) , max( prange(1:n,1) , pbar(1:n) + ccoeff * ( p(1:n,ihi) - pbar(1:n) ) ) )  
+                y2star       = func(p2star)
+                ineval       = ineval + 1
+                !
+                !  Contract the whole simplex.
+                !
+                if ( y(ihi) < y2star ) then
+                   do j = 1, n + 1
+                      ! bound to range
+                      p(1:n,j)  = min( prange(1:n,2) , max( prange(1:n,1) , ( p(1:n,j) + p(1:n,ilo) ) * 0.5_dp ) ) 
+                      nelminrange(1:n) = p(1:n,j)
+                      y(j)      = func(nelminrange)
+                      ineval     = ineval + 1
+                   end do
+                   ilo = minloc(y(1:n+1), 1)
+                   ylo = y(ilo)
+
+                   cycle
+                   !
+                   !  Retain contraction.
+                   !
+                else
+                   p(1:n,ihi) = p2star(1:n)
+                   y(ihi)     = y2star
+                end if
+                !
+                !  Contraction on the reflection side of the centroid.
+                !
+             else if ( l == 1 ) then
+
+                ! bound to range
+                p2star(1:n) =  min( prange(1:n,2) , max( prange(1:n,1) , pbar(1:n) + ccoeff * ( pstar(1:n) - pbar(1:n) ) ) ) 
+                y2star      = func(p2star)
+                ineval      = ineval + 1
+                !
+                !  Retain reflection?
+                !
+                if ( y2star <= ystar ) then
+                   p(1:n,ihi) = p2star(1:n)
+                   y(ihi)     = y2star
+                else
+                   p(1:n,ihi) = pstar(1:n)
+                   y(ihi)     = ystar
+                end if
+
+             end if
+
+          end if
+          !
+          !  Check if YLO improved.
+          !
+          if ( y(ihi) < ylo ) then
+             ylo = y(ihi)
+             ilo = ihi
+          end if
+
+          jcount = jcount - 1
+
+          if ( 0 < jcount ) cycle
+          !
+          !  Check to see if minimum reached.
+          !
+          if ( ineval <= imaxeval ) then
+             jcount = ikonvge
+             x = sum ( y(1:n+1) ) / dnn
+             z = sum ( ( y(1:n+1) - x )**2 )
+             if ( z <= rq ) exit
+          end if
+
+       end do
+       !
+       !  Factorial tests to check that FUNCMIN is a local minimum.
+       !
+       ! bound to range
+       nelminrange(1:n) = min( prange(1:n,2) , max( prange(1:n,1) , p(1:n,ilo) ) ) 
+       ifuncmin   = y(ilo)
+
+       if ( imaxeval < ineval ) then
+          iierror = 2
+          exit
+       end if
+
+       iierror = 0
+
+       do i = 1, n
+          del     = istep(i) * eps
+          ! bound to range: probably not necessary
+          nelminrange(i) = min( prange(i,2) , max( prange(i,1) , nelminrange(i) + del ) ) 
+          z       = func(nelminrange)
+          ineval  = ineval + 1
+          if ( z < ifuncmin ) then
+             iierror = 2
+             exit
+          end if
+          ! bound to range: probably not necessary
+          nelminrange(i) = min( prange(i,2) , max( prange(i,1) , nelminrange(i) - del - del ) ) 
+          z       = func(nelminrange)
+          ineval  = ineval + 1
+          if ( z < ifuncmin ) then
+             iierror = 2
+             exit
+          end if
+          ! bound to range: probably not necessary
+          nelminrange(i) = min( prange(i,2) , max( prange(i,1) , nelminrange(i) + del ) ) 
+       end do
+
+       if ( iierror == 0 ) exit
+       !
+       !  Restart the procedure.
+       !
+       ! bound to range: probably not necessary
+       ipstart(1:n) = min( prange(1:n,2) , max( prange(1:n,1) , nelminrange(1:n) ) ) 
+       del          = eps
+       inumrestart  = inumrestart + 1
+
+    end do
+
+    if (present(funcmin)) then
+       funcmin = ifuncmin
+    endif
+    if (present(neval)) then
+       neval = ineval
+    endif
+    if (present(numrestart)) then
+       numrestart = inumrestart
+    endif
+    if (present(ierror)) then
+       ierror = iierror
+    endif
+
+  end function nelminrange
+
 
   ! ------------------------------------------------------------------
 
