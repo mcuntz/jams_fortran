5266b5b0a4ebef6bbc4aec3c034469a23b57fc38
Juliena Mai <juliane.mai@ufz.de>
2012-11-20 10:59:36 +0000

    FORTRAN_lib: updating main.f90 of the lib
    git-svn-id: https://svn.ufz.de/svn/chs-svn/FORTRAN_chs_lib@469 20e3f476-f421-4892-ba25-73c5d2d2c40b

--- main.f90
+++ main.f90
@@ -1,10 +1,13 @@
 PROGRAM main
 
   USE mo_anneal
-  USE mo_nelmin
+  USE mo_append
   USE mo_boxcox
   USE mo_constants
   USE mo_corr
+  USE mo_dds
+  USE mo_elemeffects
+  USE mo_errormeasures
   USE mo_file_utils
   USE mo_finish
   USE mo_fit
@@ -16,15 +19,22 @@ PROGRAM main
   USE mo_message
   USE mo_minpack
   USE mo_moment
+  USE mo_mvn
   USE mo_ncread
   USE mo_ncwrite
+  USE mo_nelmin
   USE mo_nml
   USE mo_nr
   USE mo_nrutil
+  USE mo_ode_solver
+  USE mo_opt_functions
   USE mo_percentile
+  USE mo_poly
+  USE mo_remap
   USE mo_sort
   USE mo_string_utils
 !  USE mo_template
+  USE mo_timer
   USE mo_xor4096
 
   IMPLICIT NONE
