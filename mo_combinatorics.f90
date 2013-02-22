258f5f8ef0a1ae527d67d4b15f1cb87dde7d4b51
Juliena Mai <juliane.mai@ufz.de>
2013-02-22 20:13:43 +0000

    fortran: mo_combinatorics: routines added for select subsets from numbers 1..n: (1) generate subset randomly, (2) generate next subset to a given one, (3) generate all subsets; mainly routines from Nijenhuis et al. (1978) adapted with help of Giovanni
    git-svn-id: https://svn.ufz.de/svn/chs-svn/FORTRAN_chs_lib@885 20e3f476-f421-4892-ba25-73c5d2d2c40b

--- mo_combinatorics.f90
+++ mo_combinatorics.f90
@@ -4,12 +4,12 @@
 
 !> \details This package provides routines and functions for combinatorial calculations.
 
-!> \authors Matthias Cuntz, Juliane Mai
+!> \authors Matthias Cuntz, Giovanni Dalmasso, Juliane Mai, Stephan Thober
 !> \date Feb 2013
 
 MODULE mo_combinatorics
 
-  ! Written  Matthias Cuntz, Juliane Mai, Feb 2013
+  ! Written  Matthias Cuntz, Giovanni Dalmasso, Juliane Mai, Stephan Thober Feb 2013
   ! Modified 
 
   ! License
@@ -29,7 +29,7 @@ MODULE mo_combinatorics
   ! You should have received a copy of the GNU Lesser General Public License
   ! along with the UFZ Fortran library. If not, see <http://www.gnu.org/licenses/>.
 
-  ! Copyright 2011-2013 Matthias Cuntz, Juliane Mai
+  ! Copyright 2011-2013 Matthias Cuntz, Giovanni Dalmasso, Juliane Mai, Stephan Thober
 
 
   ! Note on Numerical Recipes License
@@ -67,9 +67,9 @@ MODULE mo_combinatorics
   IMPLICIT NONE
 
   PUBLIC :: binomcoeffi     ! Binomial coefficient (n choose k)
-  ! PUBLIC :: random_kofn    ! Random selection of k of n
-  ! PUBLIC :: next_kofn      ! Next selection of k of n to a given one
-  ! PUBLIC :: all_kofn       ! All selections of k of n
+  PUBLIC :: random_kofn    ! Random selection of k of n
+  PUBLIC :: next_kofn      ! Next selection of k of n to a given one
+  PUBLIC :: all_kofn       ! All selections of k of n
   ! PUBLIC :: random_permut  ! Random permutation to a given one
   ! PUBLIC :: next_permut    ! Next permutation to a given one
   ! PUBLIC :: all_permut     ! All permutations of n
@@ -128,6 +128,178 @@ MODULE mo_combinatorics
   INTERFACE binomcoeffi
      MODULE PROCEDURE binomcoeffi_i4_d0, binomcoeffi_i8_d0, binomcoeffi_i4_d1, binomcoeffi_i8_d1
   END INTERFACE binomcoeffi
+
+  ! ------------------------------------------------------------------
+
+  !     NAME
+  !         all_kofn
+
+  !     PURPOSE
+  !
+  !>        \brief All possible selections of k numbers from n.
+  !
+  !>        \details Returns all possible selections of a k-subset from n numbers.
+  !
+  !     INTENT(IN)
+  !>        \param[in] "integer(i4/i8) :: n"                 select from n numbers ...
+  !>        \param[in] "integer(i4/i8) :: k"                 ... k numbers
+  !
+  !     INTENT(INOUT)
+  !         None
+
+  !     INTENT(OUT)
+  !         None
+  !
+  !     INTENT(IN), OPTIONAL
+  !         None
+  !
+  !     INTENT(INOUT), OPTIONAL
+  !         None
+  !
+  !     INTENT(OUT), OPTIONAL
+  !         None
+  !
+  !     RETURN
+  !>        \return     integer(i4/i8), allocatable, dimension(:,:) :: all &mdash; All selections. Dim_2=k.
+  !
+  !     RESTRICTIONS
+  !         None
+  !
+  !     EXAMPLE
+  !         all = all_kofn(3, 2)
+  !         all --> (/ (/ 1,2 /), (/ 1,3 /), (/ 2,3 /) /) 
+  !         -> see also example in test directory
+
+  !     LITERATURE
+  !         Nijenhuis, A., & Wilf, H. S. (1978). 
+  !         Combinatorial algorithms for computers and calculators (2nd ed.). 
+  !         Academic Pr.
+
+  !     HISTORY
+  !>        \author Juliane Mai
+  !>        \date Feb 2013
+  !         Modified, 
+
+  INTERFACE all_kofn
+     MODULE PROCEDURE all_kofn_i4, all_kofn_i8
+  END INTERFACE all_kofn
+
+  ! ------------------------------------------------------------------
+
+  !     NAME
+  !         next_kofn
+
+  !     PURPOSE
+  !
+  !>        \brief The next selection of k numbers from n to a given one.
+  !
+  !>        \details Determines the next selection of k numbers from k to a given one./n
+  !>                 If one has n=5 numbers and want to pick k=3, the first possible selection is (1,2,3).
+  !>                 The next selection will be (1,2,4). The subsequent selection to (1,3,4) will be (1,3,5)./n
+  !>                 The given subset (previous) has to be sorted.
+  !
+  !     INTENT(IN)
+  !>        \param[in] "integer(i4/i8) :: n"                 select from n numbers ...
+  !>        \param[in] "integer(i4/i8) :: k"                 ... k numbers
+  !>        \param[in] "integer(i4/i8) :: previous(:)"       previous selection (dim_1 = k)
+  !
+  !     INTENT(INOUT)
+  !         None
+
+  !     INTENT(OUT)
+  !         None
+  !
+  !     INTENT(IN), OPTIONAL
+  !         None
+  !
+  !     INTENT(INOUT), OPTIONAL
+  !         None
+  !
+  !     INTENT(OUT), OPTIONAL
+  !         None
+  !
+  !     RETURN
+  !>        \return     integer(i4/i8) :: next(size(previous)) &mdash; Next selection of k from n numbers
+  !
+  !     RESTRICTIONS
+  !>        \note The given subset (previous) has to be sorted.
+  !
+  !     EXAMPLE
+  !         next = next_kofn(5, 3, (/1,3,4/))
+  !         next --> (/ 1,3,5 /)
+  !         -> see also example in test directory
+
+  !     LITERATURE
+  !         Nijenhuis, A., & Wilf, H. S. (1978). 
+  !         Combinatorial algorithms for computers and calculators (2nd ed.). 
+  !         Academic Pr.
+
+  !     HISTORY
+  !>        \author Giovanni Dalmasso, Juliane Mai
+  !>        \date Feb 2013
+  !         Modified, 
+
+  INTERFACE next_kofn
+     MODULE PROCEDURE next_kofn_i4, next_kofn_i8
+  END INTERFACE next_kofn
+
+  ! ------------------------------------------------------------------
+
+  !     NAME
+  !         random_kofn
+
+  !     PURPOSE
+  !
+  !>        \brief A random selection of a k-subset of n numbers (1..n).
+  !
+  !>        \details Returns a random k-subset of n numbers (1..n)/n
+  !>                 The returned subset will be sorted./n
+  !>                 The code is adapted from Nijenhuis (1978) - routine RANKSB.
+  !
+  !     INTENT(IN)
+  !>        \param[in] "integer(i4/i8) :: n"                 select from n numbers ...
+  !>        \param[in] "integer(i4/i8) :: k"                 ... k numbers
+  !
+  !     INTENT(INOUT)
+  !         None
+
+  !     INTENT(OUT)
+  !         None
+  !
+  !     INTENT(IN), OPTIONAL
+  !         None
+  !
+  !     INTENT(INOUT), OPTIONAL
+  !>        \param[in,out] "integer(i4/i8), optional :: save_state(n_save_state)"  an array for saving the state 
+  !>                                                                               of an uniform random number stream
+  !
+  !     INTENT(OUT), OPTIONAL
+  !         None
+  !
+  !     RETURN
+  !>        \return     integer(i4/i8) :: set(k) &mdash; Random selection of k from n numbers
+  !
+  !     RESTRICTIONS
+  !>        \note A random number stream for generating uniform random numbers has to be initialized before running this function. 
+  !
+  !     EXAMPLE
+  !         set = random_kofn(5, 3)
+  !         set --> (/ 2,3,5 /)
+  !         -> see also example in test directory
+
+  !     LITERATURE
+  !         Nijenhuis, A., & Wilf, H. S. (1978). 
+  !         Combinatorial algorithms for computers and calculators (2nd ed.). 
+  !         Academic Pr.
+
+  !     HISTORY
+  !>        \author Juliane Mai
+  !>        \date Feb 2013
+  !         Modified, 
+
+  INTERFACE random_kofn
+     MODULE PROCEDURE random_kofn_i4 , random_kofn_i8
+  END INTERFACE random_kofn
 
   ! ------------------------------------------------------------------
 
@@ -198,6 +370,371 @@ CONTAINS
     binomcoeffi_i8_d1 = nint(exp(factln(n)-factln(k)-factln(n-k)))
 
   END FUNCTION binomcoeffi_i8_d1
+
+  FUNCTION all_kofn_i4(n, k) result(all)
+
+    IMPLICIT NONE
+
+    INTEGER(I4),                         INTENT(IN)  :: n             ! from n numbers will be selected
+    INTEGER(I4),                         INTENT(IN)  :: k             ! k numbers will be selected
+    INTEGER(I4), DIMENSION(:,:), allocatable         :: all           ! all subsets
+
+    ! local variables
+    integer(i4)         :: bico
+    integer(i4)         :: i
+
+    bico = binomcoeffi(n,k)
+    allocate(all(bico,k))
+
+    do i=1,k
+       all(1,i) = i
+    end do
+
+    do i=2,bico
+       all(i,:) = next_kofn(n, k,  all(i-1,:))
+    end do
+
+  END FUNCTION all_kofn_i4
+
+  FUNCTION all_kofn_i8(n, k) result(all)
+
+    IMPLICIT NONE
+
+    INTEGER(I8),                         INTENT(IN)  :: k             ! k numbers will be selected
+    INTEGER(I8),                         INTENT(IN)  :: n             ! from n numbers will be selected
+    INTEGER(I8), DIMENSION(:,:), allocatable         :: all           ! all subsets
+
+    ! local variables
+    integer(i8)         :: bico
+    integer(i8)         :: i
+
+    bico = binomcoeffi(n,k)
+    allocate(all(bico,k))
+
+    do i=1,k
+       all(1,i) = i
+    end do
+
+    do i=2,bico
+       all(i,:) = next_kofn(n, k,  all(i-1,:))
+    end do
+
+  END FUNCTION all_kofn_i8
+
+  FUNCTION next_kofn_i4(n, k, previous) result(next)
+
+    IMPLICIT NONE
+
+    INTEGER(I4),                         INTENT(IN)  :: k             ! k numbers will be selected
+    INTEGER(I4),                         INTENT(IN)  :: n             ! from n numbers will be selected
+    INTEGER(I4), DIMENSION(k),           INTENT(IN)  :: previous      ! previous subset
+    INTEGER(I4), DIMENSION(k)                        :: next          ! next subset
+
+    ! local variables
+    integer(i4)         :: indx
+    logical             :: found
+    integer(i4)         :: i
+
+    ! find index from the back to start changing the values
+    indx = k
+    found = .false.
+    do while ( (.not. found) .and. (indx .gt. 1) ) 
+       if ( previous(indx) .eq. n-k+indx ) then
+          indx = indx-1
+       else
+          found = .true.
+       end if
+    end do
+
+    if( indx .gt. 1) then
+       ! it is a subset in between
+       next = previous
+       next(indx) = previous(indx) + 1
+       do i=indx+1,k
+          next(i) = next(i-1) + 1
+       end do
+    else
+       ! there do not exist a next subset and first one (/1, ..., k/) is returned
+       do i=1, k
+          next(i) = i
+       end do
+    end if        
+
+  END FUNCTION next_kofn_i4
+
+  FUNCTION next_kofn_i8(n, k, previous) result(next)
+
+    IMPLICIT NONE
+
+    INTEGER(I8),                         INTENT(IN)  :: k             ! k numbers will be selected
+    INTEGER(I8),                         INTENT(IN)  :: n             ! from n numbers will be selected
+    INTEGER(I8), DIMENSION(k),           INTENT(IN)  :: previous      ! previous subset
+    INTEGER(I8), DIMENSION(k)                        :: next          ! next subset
+
+    ! local variables
+    integer(i8)         :: indx
+    logical             :: found
+    integer(i8)         :: i
+
+    ! find index from the back to start changing the values
+    indx = k
+    found = .false.
+    do while ( (.not. found) .and. (indx .gt. 1) ) 
+       if ( previous(indx) .eq. n-k+indx ) then
+          indx = indx-1
+       else
+          found = .true.
+       end if
+    end do
+
+    if( indx .gt. 1) then
+       ! it is a subset in between
+       next = previous
+       next(indx) = previous(indx) + 1
+       do i=indx+1,k
+          next(i) = next(i-1) + 1
+       end do
+    else
+       ! there do not exist a next subset and first one (/1, ..., k/) is returned
+       do i=1, k
+          next(i) = i
+       end do
+    end if        
+
+  END FUNCTION next_kofn_i8
+
+  FUNCTION random_kofn_i4(n, k, save_state) result(set)
+    
+    use mo_xor4096, only: xor4096, n_save_state
+
+    implicit none
+
+    INTEGER(I4),                                    INTENT(IN)    :: k             ! k numbers will be selected
+    INTEGER(I4),                                    INTENT(IN)    :: n             ! from n numbers will be selected
+    INTEGER(I4), DIMENSION(n_save_state), OPTIONAL, INTENT(INOUT) :: save_state    ! for saving state of a uniform, sp 
+    !                                                                              ! random number stream
+    INTEGER(I4), DIMENSION(k)                                     :: set           ! random subset
+
+    ! local variables
+    logical     :: gout
+    integer(i4) :: x, r, ds, p, s, c, i, l, m,m0, iseed
+    real(sp)    :: rn
+
+    iseed = 0_i4
+
+    ! (A)
+    c=k
+    do  i=1,k
+       set(i)=(i-1)*n/k
+    end do
+    
+    ! (B)
+    do while (c .gt. 0)
+       if (present(save_state)) then
+          call xor4096(iSeed, rn, save_state=save_state)
+       else
+          call xor4096(iSeed, rn)
+       end if
+       x = 1 + int( real(n,sp)*rn, i4 )
+       l = 1 + int( real(x*k-1,sp)/real(n,sp), i4 )
+       if(x.le.set(l)) cycle
+       set(l) = set(l) + 1
+       c = c - 1
+    end do
+    i = 0
+    p = 0
+    s = k
+
+    ! (C)
+    gout = .false.
+    do while (.not. gout)
+       i = i+1
+       if (i .gt. k) then
+          gout = .true.
+       else
+          if (set(i) .eq. int(real((i-1)*n,sp)/real(k),i4)) then
+             set(i) = 0
+          else
+             p=p+1
+             m=set(i)
+             set(i) = 0
+             set(p) = m
+          end if
+       end if
+    end do
+
+    ! (D)
+    gout = .false.
+    do while (.not. gout)
+       l  = 1+int(real(set(p)*k-1,sp)/real(n,sp),i4)
+       ds = set(p) - int( real((l-1)*n,sp)/real(k,sp) ,i4)
+       set(p) = 0
+       set(s) = l
+       s=s-ds
+       p=p-1
+       if (p .le. 0) gout = .true.
+    end do
+    l = k
+     
+    do while( l .ne. 0)
+       
+       ! (E)
+       if (set(l) .ne. 0) then
+          r = l
+          m0 = 1+int(real((set(l)-1)*n,sp)/real(k,sp),i4)
+          m = int(real(set(l)* n, sp) / real(k,sp),i4) - m0 + 1
+       end if
+       
+       ! (F)
+       if (present(save_state)) then
+          call xor4096(iSeed, rn, save_state=save_state)
+       else
+          call xor4096(iSeed, rn)
+       end if
+       x = m0 + int(real(m,sp) * rn,i4)
+       i = l
+       
+       ! (G)
+       gout = .false.
+       do while (.not. gout)
+          i = i + 1
+          if (i .le. r) then
+             if( x .ge. set(i)) then
+                set(i-1) = set(i)
+                x = x+1
+             else 
+                gout = .true.
+             end if
+          else
+             gout = .true.
+          end if
+       end do
+       
+       ! (H)
+       set(i-1) = x
+       m = m-1
+       l=l-1
+
+    end do
+
+  END FUNCTION random_kofn_i4
+
+  FUNCTION random_kofn_i8(n, k, save_state) result(set)
+    
+    use mo_xor4096, only: xor4096, n_save_state
+
+    implicit none
+
+    INTEGER(I8),                                    INTENT(IN)    :: k             ! k numbers will be selected
+    INTEGER(I8),                                    INTENT(IN)    :: n             ! from n numbers will be selected
+    INTEGER(I8), DIMENSION(n_save_state), OPTIONAL, INTENT(INOUT) :: save_state    ! for saving state of a uniform, dp 
+    !                                                                              ! random number stream
+    INTEGER(I8), DIMENSION(k)                                     :: set           ! random subset
+
+    ! local variables
+    logical     :: gout
+    integer(i8) :: x, r, ds, p, s, c, i, l, m,m0, iseed
+    real(dp)    :: rn
+
+    iseed = 0_i8
+
+    ! (A)
+    c=k
+    do  i=1,k
+       set(i)=(i-1)*n/k
+    end do
+    
+    ! (B)
+    do while (c .gt. 0)
+       if (present(save_state)) then
+          call xor4096(iSeed, rn, save_state=save_state)
+       else
+          call xor4096(iSeed, rn)
+       end if
+       x = 1 + int( real(n,dp)*rn, i8 )
+       l = 1 + int( real(x*k-1,dp)/real(n,dp), i8 )
+       if(x.le.set(l)) cycle
+       set(l) = set(l) + 1
+       c = c - 1
+    end do
+    i = 0
+    p = 0
+    s = k
+
+    ! (C)
+    gout = .false.
+    do while (.not. gout)
+       i = i+1
+       if (i .gt. k) then
+          gout = .true.
+       else
+          if (set(i) .eq. int(real((i-1)*n,dp)/real(k),i8)) then
+             set(i) = 0
+          else
+             p=p+1
+             m=set(i)
+             set(i) = 0
+             set(p) = m
+          end if
+       end if
+    end do
+
+    ! (D)
+    gout = .false.
+    do while (.not. gout)
+       l  = 1+int(real(set(p)*k-1,dp)/real(n,dp),i8)
+       ds = set(p) - int( real((l-1)*n,dp)/real(k,dp) ,i8)
+       set(p) = 0
+       set(s) = l
+       s=s-ds
+       p=p-1
+       if (p .le. 0) gout = .true.
+    end do
+    l = k
+     
+    do while( l .ne. 0)
+       
+       ! (E)
+       if (set(l) .ne. 0) then
+          r = l
+          m0 = 1+int(real((set(l)-1)*n,dp)/real(k,dp),i8)
+          m = int(real(set(l)* n, dp) / real(k,dp),i8) - m0 + 1
+       end if
+       
+       ! (F)
+       if (present(save_state)) then
+          call xor4096(iSeed, rn, save_state=save_state)
+       else
+          call xor4096(iSeed, rn)
+       end if
+       x = m0 + int(real(m,dp) * rn,i8)
+       i = l
+       
+       ! (G)
+       gout = .false.
+       do while (.not. gout)
+          i = i + 1
+          if (i .le. r) then
+             if( x .ge. set(i)) then
+                set(i-1) = set(i)
+                x = x+1
+             else 
+                gout = .true.
+             end if
+          else
+             gout = .true.
+          end if
+       end do
+       
+       ! (H)
+       set(i-1) = x
+       m = m-1
+       l=l-1
+
+    end do
+
+  END FUNCTION random_kofn_i8
+
 
   ! ------------------------------------------------------------------
   ! PRIVATE
--- test/test_mo_combinatorics/main.f90
+++ test/test_mo_combinatorics/main.f90
@@ -5,13 +5,20 @@
 !*******************************************************
 program combinatorics
 
-  use mo_kind,          only: i4
-  use mo_combinatorics, only: binomcoeffi
+  use mo_kind,          only: i4, sp
+  use mo_combinatorics, only: binomcoeffi, next_kofn, all_kofn, random_kofn
+  use mo_xor4096,       only: xor4096, n_save_state
 
   implicit none
 
-  integer(i4) :: n_i4,k_i4
   integer(i4) :: c_i4
+  integer(i4) :: i
+  integer(i4) :: next_i4(3)
+  integer(i4) :: iseed
+  real(sp)    :: rn
+  integer(i4), dimension(n_save_state) :: save_state_i4
+  integer(i4) :: random_i4(3)
+  integer(i4), allocatable :: all_i4(:,:)
 
   logical :: isgood
 
@@ -20,15 +27,49 @@ program combinatorics
   write(*,*) ''
 
   !--------------------------------------------------
-  ! Test Binomial Coefficient
+  ! Test: Binomial Coefficient
   !--------------------------------------------------
 
-  n_i4 = 5_i4
-  k_i4 = 3_i4
-  c_i4 = binomcoeffi(n_i4,k_i4)
-  write(*,*) '5 choose 3 = ', c_i4
+  c_i4 = binomcoeffi(5, 3)
 
+  write(*,*) 'BINOMCOEFFI (5,3)                : ', c_i4
   if ( c_i4 .ne. 10_i4 ) isgood = .false.
+
+  !--------------------------------------------------
+  ! Test: Next k of n subset
+  !--------------------------------------------------
+
+  next_i4 = next_kofn(5,3,(/1,3,5/))
+
+  write(*,*) 'NEXT_KOFN subset after (/1,3,5/) : ', next_i4
+  if ( any(next_i4 .ne. (/ 1_i4, 4_i4, 5_i4 /)) ) isgood = .false.
+
+  !--------------------------------------------------
+  ! Test: All k of n subset
+  !--------------------------------------------------
+
+  all_i4 = all_kofn(5,3)
+
+  write(*,*) 'ALL_KOFN (5,3)                  : '
+  do i=1,size(all_i4,1)
+     write(*,*) '                              subset #',i,'   ',all_i4(i,:)
+  end do
+  if ( any(all_i4(5,:) .ne. (/ 1_i4, 3_i4, 5_i4 /)) ) isgood = .false.
+
+  !--------------------------------------------------
+  ! Test: Random k of n subset
+  !--------------------------------------------------
+
+  iseed=1045_i4
+  call xor4096(iSeed, rn, save_state=save_state_i4)
+
+  write(*,*) 'RANDOM_KOFN (5,3)               : '
+  do i=1,6
+     random_i4 = random_kofn(5,3,save_state=save_state_i4)
+     write(*,*) '                              subset #',i,'   ',random_i4
+  end do
+  if ( any(random_i4(:) .ne. (/ 3_i4, 4_i4, 5_i4 /)) ) isgood = .false.
+
 
   write(*,*) ''
   if (isgood) then
