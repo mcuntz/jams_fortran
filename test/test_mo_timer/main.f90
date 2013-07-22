d0984cba8bc85da69b0d2db331250fee7002eb9a
Matthias Cuntz <matthias.cuntz@ufz.de>
2013-07-22 19:49:30 +0000

    Fortran: test all functions and variables in mo_timer.f90
    git-svn-id: https://svn.ufz.de/svn/chs-svn/FORTRAN_chs_lib@1100 20e3f476-f421-4892-ba25-73c5d2d2c40b

--- test/test_mo_timer/main.f90
+++ test/test_mo_timer/main.f90
@@ -1,7 +1,8 @@
 program main
 
   use mo_kind,  only: i4, dp
-  use mo_timer, only: max_timers, timers_init, timer_clear, timer_start, timer_stop, timer_get
+  use mo_timer, only: max_timers, timers_init, timer_clear, timer_start, timer_stop, timer_get, &
+       max_timers, cycles_max, clock_rate, cycles1, cycles2, cputime, status, timer_check, timer_print
 
   implicit none
 
@@ -20,6 +21,8 @@ program main
   do i=1, 10
      r = r**(1./real(i,dp))
   end do
+  call timer_check(1)
+  call timer_print(1)
   call timer_stop(1)
   !
   ! Do something else
@@ -27,6 +30,8 @@ program main
   do i=1, 10
      r = exp(sin(r))
   end do
+  call timer_check(2)
+  call timer_print(2)
   call timer_stop(2)
   !
   ! Wrte out timings
@@ -34,6 +39,21 @@ program main
   write(*,*) 'Timings [s]'
   write(*,*) 'Something done: ', timer_get(1)
   write(*,*) 'Something else: ', timer_get(2)
+  !
+  ! test a bit more
+  write(*,*) 'max_timers ', max_timers
+  write(*,*) 'cycles_max ', cycles_max
+  write(*,*) 'clock_rate ', clock_rate
+  write(*,*) 'cycles1    ', cycles1   
+  write(*,*) 'cycles2    ', cycles2   
+  write(*,*) 'cputime    ', cputime   
+  write(*,*) 'status     ', status
+  do i=1, 2
+     call timer_check(i)
+  end do
+  do i=1, 2
+     call timer_print(i)
+  end do
   !
   write(*,*) 'mo_timer single precision o.k.'
   !
