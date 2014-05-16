4163adabe4be2cabcb693b4bd7ee79ccf21278ad
Oldrich Rakovec <oldrich.rakovec@ufz.de>
2014-05-16 09:29:50 +0000

    Fortran: adding delsa module
    git-svn-id: https://svn.ufz.de/svn/chs-svn/FORTRAN_chs_lib@1551 20e3f476-f421-4892-ba25-73c5d2d2c40b

---
+++ mo_delsa.f90
@@ -0,0 +1,231 @@
+!> \file mo_delsa.f90
+
+!> \brief Distributed Evaluation of Local Sensitivity Analysis
+
+!> \details This module calculates first order parameter sensitivity 
+!>           using DELSA (Rakovec et al., 2014, WRR).
+
+!> \authors Oldrich Rakovec
+!> \date May 2014
+
+MODULE mo_delsa
+
+  ! This module calculates first order parameter sensitivity using DELSA (Rakovec et al., 2014, WRR) 
+
+  ! Written  Oldrich Rakovec, May 2014
+  ! Modified 
+
+  ! License
+  ! -------
+  ! This file is part of the UFZ Fortran library.
+
+  ! The UFZ Fortran library is free software: you can redistribute it and/or modify
+  ! it under the terms of the GNU Lesser General Public License as published by
+  ! the Free Software Foundation, either version 3 of the License, or
+  ! (at your option) any later version.
+
+  ! The UFZ Fortran library is distributed in the hope that it will be useful,
+  ! but WITHOUT ANY WARRANTY; without even the implied warranty of
+  ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  ! GNU Lesser General Public License for more details.
+
+  ! You should have received a copy of the GNU Lesser General Public License
+  ! along with the UFZ Fortran library. If not, see <http://www.gnu.org/licenses/>.
+
+  ! Copyright 204 Oldrich Rakovec
+
+  USE mo_kind, ONLY: i4, sp, dp
+
+  IMPLICIT NONE
+
+  PUBLIC :: delsa   ! delsa = distributed evaluation of local sensitivity analysis
+
+  ! ------------------------------------------------------------------
+  !     NAME
+  !         delsa
+
+  !     PURPOSE
+  !         Calculates first order parameter sensitivity using the DELSA method thoughout the parameter space
+  !
+  !>        \brief DELSA.
+  !
+  !>        \details Calculates first order parameter sensitivity using the DELSA method thoughout the parameter space:
+  !>        S_{L1}^j=\frac{\left(\frac{\partial \Psi_l}{\partial \theta_j} \right)^2 s_j^2}{V_L(\Psi)}
+  !>        for more details see derivation of equation (13) in Rakovec et al. (2014, WRR)
+  !
+  !
+  !     INTENT(IN)
+  !>        \param[in] "real(sp/dp) :: parbase(:,:)"    array of base model parameters, first dimension is the sample size, 
+  !>                                                    i.e. number of parameter sets, second dimension is the number of 
+  !>                                                    parameters, which are recommended to be sampled using the Sobol' 
+  !>                                                    quasi random sequence (see for example mo_sobol in FORTRAN_chs_lib)         
+  !>        \param[in] "real(sp/dp) :: parpert(:,:)"    array of perturbed model parameters, first dimension is the sample size, 
+  !>                                                    i.e. number of parameter sets, second dimension is the number of 
+  !>                                                    parameters, which are recommended to be sampled using the Sobol' 
+  !>                                                    quasi random sequence (see mo_sobol), perturbation e.g. 1% parameter change
+  !>                                                    as often done in literature (e.g. Hill, M. C. and C. R. Tiedeman; 2007)
+  !>        \param[in] "real(sp/dp) :: outbase(:)"      baserun model output obtained using parbase(:,:) 
+  !>        \param[in] "real(sp/dp) :: outpert(:,:)"    perturbed model output obtained using parpert(:,:) 
+  !>        \param[in] "real(sp/dp) :: varprior(:)"     prior variance obtained using equation for uniform distribution: 
+  !>                                                    $\frac{1}{12}(\theta_{j,max}-\theta_{j,min})^2$
+
+  !
+  !     INTENT(INOUT)
+  !         None
+
+  !     INTENT(OUT)
+  !>        \param[out] "real(sp/dp) :: delsafirst(:,:)" delsa, first order Local first order sensitivity index 
+  !>                                                     (see eq. 13 in Rakovec et al 2014), dimensions correspond to parbase(:,:)   
+
+  !
+  !     INTENT(IN), OPTIONAL
+  !         None
+  !
+  !     INTENT(INOUT), OPTIONAL
+  !         None
+  !
+  !     INTENT(OUT), OPTIONAL
+  !         None
+  !
+  !     RETURN
+  !         None
+  !     
+  !     RESTRICTIONS
+  !>       \note Works only with scalar model output.\n
+  !>             No averaging of DELSA results implemented yet. \n 
+  !>             No check for outliers of the model output. \n
+  !>             No masking of parameters possible. 
+  !
+  !     EXAMPLE
+  !         -> see example in test directory
+
+  !     LITERATURE
+  !         Rakovec, O., M. C. Hill, M. P. Clark, A. H. Weerts, A. J. Teuling, R. Uijlenhoet (2014), Distributed 
+  !                Evaluation of Local Sensitivity Analysis (DELSA), with application to hydrologic models, 
+  !                Water Resour. Res., 50, 1-18, doi:10.1002/2013WR014063.
+  !         Hill, M.~C., and C.~R. Tiedeman (2007), Effective groundwater model calibration: with analysis of data,
+  !                sensitivities, prediction and uncertainty, 455 pp., Wiley.
+
+  !     HISTORY
+  !>        \author Oldrich Rakovec
+  !>        \date May 2014
+  !         Modified, 
+
+  INTERFACE delsa
+     MODULE PROCEDURE delsa_sp, delsa_dp
+  END INTERFACE delsa
+
+  ! ------------------------------------------------------------------
+
+  PRIVATE
+
+  ! ------------------------------------------------------------------
+
+CONTAINS
+
+  SUBROUTINE delsa_sp(parbase,parpert,outbase,outpert,varprior,delsafirst)
+
+    IMPLICIT NONE
+
+    REAL(sp), DIMENSION(:,:),                             INTENT(IN)  :: parbase      ! array of base model parameters
+    REAL(sp), DIMENSION(:,:),                             INTENT(IN)  :: parpert      ! array of perturbed model parameters
+    REAL(sp), DIMENSION(:),                               INTENT(IN)  :: outbase      ! model output using the base runs   
+    REAL(sp), DIMENSION(:,:),                             INTENT(IN)  :: outpert      ! model output using the perturbed runs
+    REAL(sp), DIMENSION(:),                               INTENT(IN)  :: varprior     ! prior parameter variance
+    REAL(sp), DIMENSION(size(parbase,1),size(parbase,2)), INTENT(OUT) :: delsafirst   ! first-order DELSA results
+
+    ! local variables
+    INTEGER(I4)                                                       :: Nsamp        ! sample size
+    INTEGER(I4)                                                       :: Kpar         ! number of model parameters  
+    INTEGER(I4)                                                       :: rsamp,jpar   ! counter 
+    REAL(sp), DIMENSION(size(parbase,1),size(parbase,2))              :: deriv        ! array of parameter derivatives (eq.10)
+    !                                                                                 ! see nominator of eq. 13 in Rakovec et al 2014)
+    REAL(sp), DIMENSION(size(parbase,1),size(parbase,2))              :: varfir       ! array of lcal first order variance, 
+    !                                                                                 ! see nominator of eq. 13 in Rakovec et al 2014)
+    REAL(sp), DIMENSION(size(parbase,1))                              :: vartot       ! local equation total variance (see eq. 12)
+
+    Nsamp = size(parbase,1)
+    Kpar  = size(parbase,2)
+
+    ! initialization
+    vartot(:)   = 0.0_sp
+    deriv(:,:)  = 0.0_sp
+    varfir(:,:) = 0.0_sp
+
+    loop_sets: do rsamp = 1,Nsamp ! looping over parameter sets 
+
+       loop_para: do jpar = 1,Kpar ! looping over model parameters
+
+          ! calculate derivative (see eq. 10 in Rakovec et al 2014)
+          deriv(rsamp,jpar) = (outpert(rsamp,jpar) -  outbase(rsamp)) / (parpert(rsamp,jpar) - parbase(rsamp,jpar))
+
+          ! calculate local first order variance (see nominator of eq. 13 in Rakovec et al 2014)
+          varfir(rsamp,jpar) =  (deriv(rsamp,jpar)**2.0_i4)*(varprior(jpar))
+
+          ! calculate local equation total variance (see eq. 12 in Rakovec et al 2014)
+          vartot(rsamp) = vartot(rsamp) + varfir(rsamp,jpar)
+
+       end do loop_para ! End loop over model parameters
+
+       ! finally when vartot is complete, we can calculate delsa, 
+       ! here as the first order Local first order sensitivity index (see eq. 13 in Rakovec et al 2014)
+       delsafirst(rsamp,:) =  varfir(rsamp,:) /  vartot(rsamp) 
+
+    end do loop_sets ! End loop over parameter sets
+
+  END SUBROUTINE delsa_sp
+
+
+  SUBROUTINE delsa_dp(parbase,parpert,outbase,outpert,varprior,delsafirst)
+
+    IMPLICIT NONE
+
+    REAL(dp), DIMENSION(:,:),                             INTENT(IN)  :: parbase      ! array of base model parameters
+    REAL(dp), DIMENSION(:,:),                             INTENT(IN)  :: parpert      ! array of perturbed model parameters
+    REAL(dp), DIMENSION(:),                               INTENT(IN)  :: outbase      ! model output using the base runs   
+    REAL(dp), DIMENSION(:,:),                             INTENT(IN)  :: outpert      ! model output using the perturbed runs
+    REAL(dp), DIMENSION(:),                               INTENT(IN)  :: varprior     ! prior parameter variance
+    REAL(dp), DIMENSION(size(parbase,1),size(parbase,2)), INTENT(OUT) :: delsafirst   ! first-order DELSA results
+
+    ! local variables
+    INTEGER(I4)                                                       :: Nsamp        ! sample size
+    INTEGER(I4)                                                       :: Kpar         ! number of model parameters  
+    INTEGER(I4)                                                       :: rsamp,jpar   ! counter 
+    REAL(dp), DIMENSION(size(parbase,1),size(parbase,2))              :: deriv        ! array of parameter derivatives (eq.10)
+    !                                                                                 ! see nominator of eq. 13 in Rakovec et al 2014)
+    REAL(dp), DIMENSION(size(parbase,1),size(parbase,2))              :: varfir       ! array of lcal first order variance, 
+    !                                                                                 ! see nominator of eq. 13 in Rakovec et al 2014)
+    REAL(dp), DIMENSION(size(parbase,1))                              :: vartot       ! local equation total variance (see eq. 12)
+
+    Nsamp = size(parbase,1)
+    Kpar  = size(parbase,2)
+
+    ! initialization
+    vartot(:)   = 0.0_dp
+    deriv(:,:)  = 0.0_dp
+    varfir(:,:) = 0.0_dp
+
+    loop_sets: do rsamp = 1,Nsamp ! looping over parameter sets 
+
+       loop_para: do jpar = 1,Kpar ! looping over model parameters
+
+          ! calculate derivative (see eq. 10 in Rakovec et al 2014)
+          deriv(rsamp,jpar) = (outpert(rsamp,jpar) -  outbase(rsamp)) / (parpert(rsamp,jpar) - parbase(rsamp,jpar))
+
+          ! calculate local first order variance (see nominator of eq. 13 in Rakovec et al 2014)
+          varfir(rsamp,jpar) =  (deriv(rsamp,jpar)**2.0_i4)*(varprior(jpar))
+
+          ! calculate local equation total variance (see eq. 12 in Rakovec et al 2014)
+          vartot(rsamp) = vartot(rsamp) + varfir(rsamp,jpar)
+
+       end do loop_para ! End loop over model parameters
+
+       ! finally when vartot is complete, we can calculate delsa, 
+       ! here as the first order Local first order sensitivity index (see eq. 13 in Rakovec et al 2014)
+       delsafirst(rsamp,:) =  varfir(rsamp,:) /  vartot(rsamp) 
+
+    end do loop_sets ! End loop over parameter sets
+
+  END SUBROUTINE delsa_dp
+
+END MODULE mo_delsa
---
+++ test/test_mo_delsa/main.f90
@@ -0,0 +1,127 @@
+PROGRAM main
+
+  ! This programs produces the 2-parameter toy model presented in Rakovec et al (2014, WRR)
+  ! which calculates first-order DELSA method shown in bottom Figure 5.
+
+  USE mo_kind,     ONLY: dp, sp, i8, i4
+  USE mo_delsa, ONLY: delsa
+
+  IMPLICIT NONE
+
+  integer(i4)                      :: ii,jj,set
+
+  integer(i4), parameter           :: npara = 2         ! number of parameter
+  integer(i4), parameter           :: nsets = 100       ! number of parametersets 
+
+  REAL(dp), DIMENSION(nsets,npara) :: parbase           ! array of base model parameters
+  REAL(dp), DIMENSION(nsets,npara) :: parpert           ! array of perturbed model parameters (alltogether)
+  REAL(dp), DIMENSION(nsets,npara) :: parpert1,parpert2 ! array of perturbed model parameters (individual param)
+  REAL(dp), DIMENSION(nsets)       :: outbase           ! model output using the base runs   
+  REAL(dp), DIMENSION(nsets,npara) :: outpert           ! model output using the perturbed runs
+  REAL(dp), DIMENSION(npara)       :: varprior          ! prior parameter variance
+
+  REAL(dp), DIMENSION(nsets,npara) :: delsafirst,delsatest ! first-order DELSA results,delsa test results for a check
+
+  ! Dummy variables
+  logical                             :: isgood = .true.
+
+  ! Data Files for delsa(parbase,parpert,outbase,outpert,varprior,delsafirst)
+  ! read text files which were created externally:
+  character(len=*), parameter :: fileparbase    = './dummy_data/parbase.txt'  
+  character(len=*), parameter :: fileparpert1   = './dummy_data/parpert1.txt'
+  character(len=*), parameter :: fileparpert2   = './dummy_data/parpert2.txt'   
+  character(len=*), parameter :: fileoutbase    = './dummy_data/outbase.txt'
+  character(len=*), parameter :: fileoutpert    = './dummy_data/outpert.txt'
+  character(len=*), parameter :: filetestoutput    = './dummy_data/test_output.txt'
+
+  ! ************************************************************************
+  ! read parameter sets and corresponding model outputs
+  ! ************************************************************************
+
+  ! read base parameter set:
+  open(unit=50, file=fileparbase, action='read')
+  do set=1,size(parbase,1)
+     read(50,*) parbase(set,:)
+  end do
+  close(50)
+
+  ! read parameter set, in which is perturbed parameter 1:
+  open(unit=50, file=fileparpert1, action='read')
+  do set=1,size(parbase,1)
+     read(50,*) parpert1(set,:)
+  end do
+  close(50)
+
+  ! read parameter set, in which is perturbed parameter 2:
+  open(unit=50, file=fileparpert2, action='read')
+  do set=1,size(parbase,1)
+     read(50,*) parpert2(set,:)
+  end do
+  close(50)
+
+  ! compose an array with only perturbed parameters
+  parpert(:,1)=parpert1(:,1)
+  parpert(:,2)=parpert2(:,2)
+
+  ! read model output using the base parameter set
+  open(unit=50, file=fileoutbase, action='read')
+  do set=1,size(outbase,1)
+     read(50,*) outbase(set)
+  end do
+  close(50)
+
+  ! read model output using the perturbed parameter sets
+  open(unit=50, file=fileoutpert, action='read')
+  do set=1,size(outpert,1)
+     read(50,*) outpert(set,:)
+  end do
+  close(50)
+
+  ! prior variance (this comes from last sentence in Rakovec et al, 2014, WRR of Sect. 3.2)
+  ! note, there is a typo in the paper, those values are standard deviations and not variances!!!
+  varprior = (/28.83865_dp**2_i4,0.08660254_dp**2_i4/)
+  Write(*,*) 'varprior: '
+  Write(*,*) varprior
+  Write(*,*) ' '
+
+  call delsa(parbase,parpert,outbase,outpert,varprior,delsafirst)
+  Write(*,*) 'delsa first, param 1:'
+  Write(*,*) delsafirst(:,1)
+  Write(*,*) ' '
+  Write(*,*) 'delsa first, param 2:'
+  Write(*,*) delsafirst(:,2)
+  Write(*,*) ' '
+
+  ! save results in a text file: 
+  open(unit=999,file='delsafirst_fortran.txt.make_check_test_file', action='write')
+
+  do set=1,size(outbase,1)
+     write(999,'(f12.6)', advance='no') delsafirst(set,1)
+     write(999,'(f12.6)') delsafirst(set,2)
+  end do
+  close(999)
+  Write(*,*) 'results saved in delsafirst_fortran.txt.make_check_test_file'
+
+
+  ! ************************************************************************
+  !       Verify that module runs correctly
+  ! ************************************************************************
+  open(unit=50, file=filetestoutput, action='read')
+  do set=1,size(delsatest,1)
+     read(50,*) delsatest(set,:)
+  end do
+  close(50)
+
+  do ii=1,size(delsatest,1)
+     do jj=1,size(delsatest,2)
+     if (nint(delsafirst(ii,jj)*1000000._dp) .ne.  nint(delsatest(ii,jj)*1000000._dp))  isgood = .false.     
+     end do
+  end do
+
+  if (isgood) then
+     write(*,*) 'mo_delsa o.k.'
+  else
+     write(*,*) 'mo_delsa failed'
+  end if
+
+END PROGRAM main
