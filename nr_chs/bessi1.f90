7b8ad7574adbd8da9ac1dba62b4abbfbd166f43b
Sebastian Mueller <sebastian.mueller@ufz.de>
2015-03-18 13:33:27 +0000

    FORTRAN_lib: bess*1.f90: fixed some sp/dp issues
    git-svn-id: https://svn.ufz.de/svn/chs-svn/FORTRAN_chs_lib@2069 20e3f476-f421-4892-ba25-73c5d2d2c40b

--- nr_chs/bessi1.f90
+++ nr_chs/bessi1.f90
@@ -5,18 +5,18 @@ FUNCTION bessi1_s(x)
   REAL(SP), INTENT(IN) :: x
   REAL(SP) :: bessi1_s
   REAL(SP) :: ax
-  REAL(DP), DIMENSION(7) :: p = (/0.5_dp,0.87890594_dp,&
-       0.51498869_dp,0.15084934_dp,0.2658733e-1_dp,&
-       0.301532e-2_dp,0.32411e-3_dp/)
-  REAL(DP), DIMENSION(9) :: q = (/0.39894228_dp,-0.3988024e-1_dp,&
-       -0.362018e-2_dp,0.163801e-2_dp,-0.1031555e-1_dp,&
-       0.2282967e-1_dp,-0.2895312e-1_dp,0.1787654e-1_dp,&
-       -0.420059e-2_dp/)
+  REAL(sp), DIMENSION(7) :: p = (/0.5_sp,0.87890594_sp,&
+       0.51498869_sp,0.15084934_sp,0.2658733e-1_sp,&
+       0.301532e-2_sp,0.32411e-3_sp/)
+  REAL(sp), DIMENSION(9) :: q = (/0.39894228_sp,-0.3988024e-1_sp,&
+       -0.362018e-2_sp,0.163801e-2_sp,-0.1031555e-1_sp,&
+       0.2282967e-1_sp,-0.2895312e-1_sp,0.1787654e-1_sp,&
+       -0.420059e-2_sp/)
   ax=abs(x)
-  if (ax < 3.75_dp) then
-     bessi1_s=ax*poly(real((x/3.75_sp)**2,dp),p)
+  if (ax < 3.75_sp) then
+     bessi1_s=ax*poly(real((x/3.75_sp)**2,sp),p)
   else
-     bessi1_s=(exp(ax)/sqrt(ax))*poly(real(3.75_sp/ax,dp),q)
+     bessi1_s=(exp(ax)/sqrt(ax))*poly(real(3.75_sp/ax,sp),q)
   end if
   if (x < 0.0_sp) bessi1_s=-bessi1_s
 END FUNCTION bessi1_s
@@ -29,22 +29,22 @@ FUNCTION bessi1_v(x)
   REAL(SP), DIMENSION(:), INTENT(IN) :: x
   REAL(SP), DIMENSION(size(x)) :: bessi1_v
   REAL(SP), DIMENSION(size(x)) :: ax
-  REAL(DP), DIMENSION(size(x)) :: y
+  REAL(sp), DIMENSION(size(x)) :: y
   LOGICAL(LGT), DIMENSION(size(x)) :: mask
-  REAL(DP), DIMENSION(7) :: p = (/0.5_dp,0.87890594_dp,&
-       0.51498869_dp,0.15084934_dp,0.2658733e-1_dp,&
-       0.301532e-2_dp,0.32411e-3_dp/)
-  REAL(DP), DIMENSION(9) :: q = (/0.39894228_dp,-0.3988024e-1_dp,&
-       -0.362018e-2_dp,0.163801e-2_dp,-0.1031555e-1_dp,&
-       0.2282967e-1_dp,-0.2895312e-1_dp,0.1787654e-1_dp,&
-       -0.420059e-2_dp/)
+  REAL(sp), DIMENSION(7) :: p = (/0.5_sp,0.87890594_sp,&
+       0.51498869_sp,0.15084934_sp,0.2658733e-1_sp,&
+       0.301532e-2_sp,0.32411e-3_sp/)
+  REAL(sp), DIMENSION(9) :: q = (/0.39894228_sp,-0.3988024e-1_sp,&
+       -0.362018e-2_sp,0.163801e-2_sp,-0.1031555e-1_sp,&
+       0.2282967e-1_sp,-0.2895312e-1_sp,0.1787654e-1_sp,&
+       -0.420059e-2_sp/)
   ax=abs(x)
   mask = (ax < 3.75_sp)
   where (mask)
-     bessi1_v=ax*poly(real((x/3.75_sp)**2,dp),p,mask)
+     bessi1_v=ax*poly(real((x/3.75_sp)**2,sp),p,mask)
   elsewhere
      y=3.75_sp/ax
-     bessi1_v=(exp(ax)/sqrt(ax))*poly(real(y,dp),q,.not. mask)
+     bessi1_v=(exp(ax)/sqrt(ax))*poly(real(y,sp),q,.not. mask)
   end where
   where (x < 0.0_sp) bessi1_v=-bessi1_v
 END FUNCTION bessi1_v
--- nr_chs/bessk1.f90
+++ nr_chs/bessk1.f90
@@ -5,13 +5,13 @@ FUNCTION bessk1_s(x)
   IMPLICIT NONE
   REAL(SP), INTENT(IN) :: x
   REAL(SP) :: bessk1_s
-  REAL(DP) :: y
-  REAL(DP), DIMENSION(7) :: p = (/1.0_dp,0.15443144_dp,&
-       -0.67278579_dp,-0.18156897_dp,-0.1919402e-1_dp,&
-       -0.110404e-2_dp,-0.4686e-4_dp/)
-  REAL(DP), DIMENSION(7) :: q = (/1.25331414_dp,0.23498619_dp,&
-       -0.3655620e-1_dp,0.1504268e-1_dp,-0.780353e-2_dp,&
-       0.325614e-2_dp,-0.68245e-3_dp/)
+  REAL(sp) :: y
+  REAL(sp), DIMENSION(7) :: p = (/1.0_sp,0.15443144_sp,&
+       -0.67278579_sp,-0.18156897_sp,-0.1919402e-1_sp,&
+       -0.110404e-2_sp,-0.4686e-4_sp/)
+  REAL(sp), DIMENSION(7) :: q = (/1.25331414_sp,0.23498619_sp,&
+       -0.3655620e-1_sp,0.1504268e-1_sp,-0.780353e-2_sp,&
+       0.325614e-2_sp,-0.68245e-3_sp/)
   call assert(x > 0.0, 'bessk1_s arg')
   if (x <= 2.0) then
      y=x*x/4.0_sp
@@ -30,14 +30,14 @@ FUNCTION bessk1_v(x)
   IMPLICIT NONE
   REAL(SP), DIMENSION(:), INTENT(IN) :: x
   REAL(SP), DIMENSION(size(x)) :: bessk1_v
-  REAL(DP), DIMENSION(size(x)) :: y
+  REAL(sp), DIMENSION(size(x)) :: y
   LOGICAL(LGT), DIMENSION(size(x)) :: mask
-  REAL(DP), DIMENSION(7) :: p = (/1.0_dp,0.15443144_dp,&
-       -0.67278579_dp,-0.18156897_dp,-0.1919402e-1_dp,&
-       -0.110404e-2_dp,-0.4686e-4_dp/)
-  REAL(DP), DIMENSION(7) :: q = (/1.25331414_dp,0.23498619_dp,&
-       -0.3655620e-1_dp,0.1504268e-1_dp,-0.780353e-2_dp,&
-       0.325614e-2_dp,-0.68245e-3_dp/)
+  REAL(sp), DIMENSION(7) :: p = (/1.0_sp,0.15443144_sp,&
+       -0.67278579_sp,-0.18156897_sp,-0.1919402e-1_sp,&
+       -0.110404e-2_sp,-0.4686e-4_sp/)
+  REAL(sp), DIMENSION(7) :: q = (/1.25331414_sp,0.23498619_sp,&
+       -0.3655620e-1_sp,0.1504268e-1_sp,-0.780353e-2_sp,&
+       0.325614e-2_sp,-0.68245e-3_sp/)
   call assert(all(x > 0.0), 'bessk1_v arg')
   mask = (x <= 2.0)
   where (mask)
