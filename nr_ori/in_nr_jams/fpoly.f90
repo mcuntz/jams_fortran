4d956d083c61e845efe90da20202f09c6e8cb30a
Matthias Cuntz <mc@macu.de>
2019-06-23 18:22:46 +0200

    Removed nr_ori from repository because numerical recipes files cannot be made public.

--- nr_ori/in_nr_jams/fpoly.f90
+++
@@ -1,9 +0,0 @@
-	FUNCTION fpoly(x,n)
-	use mo_nrtype
-          use mo_nrutil, ONLY : geop
-	IMPLICIT NONE
-	REAL(SP), INTENT(IN) :: x
-	INTEGER(I4B), INTENT(IN) :: n
-	REAL(SP), DIMENSION(n) :: fpoly
-	fpoly=geop(1.0_sp,x,n)
-	END FUNCTION fpoly
--- nr_ori/sprstp.f90
+++
@@ -1,9 +0,0 @@
-	SUBROUTINE sprstp(sa)
-	use mo_nrtype
-	IMPLICIT NONE
-	TYPE(sprs2_sp), INTENT(INOUT) :: sa
-	INTEGER(I4B), DIMENSION(:), POINTER :: temp
-	temp=>sa%irow
-	sa%irow=>sa%jcol
-	sa%jcol=>temp
-	END SUBROUTINE sprstp
